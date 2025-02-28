0a0841f51192621f5aac6ffcd10e2d29a42b99a6
Sumaiya Tabassum <tabassumoyshee@gmail.com>
2025-02-28 21:46:43 +0600

    email

--- pkg/controllers/cronjobs.go
+++ pkg/controllers/cronjobs.go
@@ -44,7 +44,7 @@ func CronJob() {
 		log.Fatalf(" Failed to schedule LunchSummaryForEmail: %v", err)
 	}
 
-	_, err = c.AddFunc("* * * * *", func() {
+	_, err = c.AddFunc("05 14 * * *", func() {
 		log.Println("Sending Snacks Summary at:", time.Now())
 		if err := MealActivityService.SnackSummaryForEmail(); err != nil {
 			log.Printf(" Error generating snacks summary: %v", err)
